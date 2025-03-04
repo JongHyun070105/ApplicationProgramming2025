044fc25406c39853f713591e5645e2670546986a
JongHyun070105 <bobi09361@gmail.com>
2025-03-04 15:15:13 +0900

    ApplicationProgramming2025

--- src/Day0304/JComponentEX.java
+++ src/Day0304/JComponentEX.java
@@ -3,5 +3,16 @@ package Day0304;
 import javax.swing.*;
 
 public class JComponentEX extends JFrame {
+    public JComponentEX() {
+        setTitle("첫번째 과제");
 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(300,300);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
