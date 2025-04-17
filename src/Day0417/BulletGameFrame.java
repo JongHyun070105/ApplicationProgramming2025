9e516e67d1290f7d43636a337cab4d2768323568
JongHyun070105 <bobi09361@gmail.com>
2025-04-17 15:18:19 +0900

    ApplicationProgramming2025

--- src/Day0417/BulletGameFrame.java
+++ src/Day0417/BulletGameFrame.java
@@ -1,175 +1,173 @@
 package Day0417;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-class GamePanel extends JPanel{
-    private TargetThread targetThread = null;
-    private JLabel baseLabel = new JLabel();
-    private JLabel bulletLabel = new JLabel();
-    private JLabel targetLabel = new JLabel();
-
-    public GamePanel(){
-        setLayout(null);
-        baseLabel.setSize(40,40);
-        baseLabel.setOpaque(true);
-        baseLabel.setBackground(Color.BLACK);
-
-        ImageIcon img = new ImageIcon("img/chicken.png");
-        targetLabel = new JLabel(img);
-        targetLabel.setSize(img.getIconWidth(),img.getIconHeight());
-
-        bulletLabel.setSize(10,10);
-        bulletLabel.setOpaque(true);
-        bulletLabel.setBackground(Color.RED);
-
-        add(baseLabel);
-        add(targetLabel);
-        add(bulletLabel);
-
-        this.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                baseLabel.setFocusable(true);
-                baseLabel.requestFocus();
-            }
-        });
-    }
-    public void startGame(){
-        baseLabel.setLocation(this.getWidth() / 2 - 20, this.getHeight() / 2 - 40);
-        bulletLabel.setLocation(this.getWidth() / 2 - 5, this.getHeight() - 50);
-        targetLabel.setLocation(0, 0);
-
-        targetThread = new TargetThread(targetLabel);
-        targetThread.start();
-
-        // GamePanel에 포커스 설정
-        setFocusable(true);
-        requestFocusInWindow(); // 이 부분을 통해 GamePanel이 키 입력을 받을 수 있도록 설정
-
-        addKeyListener(new KeyAdapter() {
-            BulletThread bulletThread = null;
-            @Override
-            public void keyPressed(KeyEvent e){
-                if(e.getKeyChar() == '\n'){
-                    // 총알이 이미 발사 중이 아니면 새로운 BulletThread를 시작
-                    if(bulletThread == null || !bulletThread.isAlive()){
-                        bulletThread = new BulletThread(targetLabel, bulletLabel, targetThread);
-                        bulletThread.start();
-                    }
-                }
-            }
-        });
-    }
-
-    class BulletThread extends Thread{
-        private JComponent target, bullet;
-        private Thread targetThread;
-        public BulletThread(JComponent target, JComponent bullet, Thread targetThread) {
-            this.bullet = bullet;
-            this.target = target;
-            this.targetThread = targetThread;
-        }
-        @Override
-        public void run(){
-            while(true){
-                if(hit()){
-                    targetThread.interrupt();
-                    bullet.setLocation(bullet.getParent().getWidth() / 2 -5, bullet.getParent().getHeight() - 50);
-                    return;
-                }else{
-                    int x = bullet.getX();
-                    int y = bullet.getY();
-                    y -= 5; // 총알을 위로 이동시킴
-                    if(y < 0){
-                        bullet.setLocation(bullet.getParent().getWidth() / 2 -5, bullet.getParent().getHeight() - 50 );
-                        bullet.getParent().repaint();
-                        return;
-                    }
-                    bullet.setLocation(x,y);
-                    bullet.getParent().repaint();
-                }
-                try{
-                    sleep(20);
-                }catch (InterruptedException e){
-                    return;
-                }
-            }
-        }
-        public boolean hit(){
-            if (targetContains(bullet.getX(), bullet.getY()) ||
-                targetContains(bullet.getX() + bullet.getWidth() - 1, bullet.getY()) ||
-                targetContains(bullet.getX() + bullet.getWidth() - 1, bullet.getY() + bullet.getHeight() - 1) ||
-                targetContains(bullet.getX(), bullet.getY() + bullet.getHeight() - 1)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        public boolean targetContains(int x, int y){
-            if(((target.getX() <= x) && (target.getX() + target.getWidth() - 1 >= x)) &&
-                    ((target.getY() <= y) && target.getY() + bullet.getWidth() - 1 >= y)){
-                return  true;
-            }else{
-                return false;
-            }
-        }
-    }
-}
-class TargetThread extends Thread{
-    private JComponent target;
-    public TargetThread(JComponent target){
-        this.target = target;
-        target.setLocation(0,0);
-        target.getParent().repaint();
-    }
-
-    @Override
-    public void run(){
-        while(true){
-            int x = target.getX() + 5;
-            int y = target.getY();
-
-            if(x > target.getParent().getWidth()){
-                target.setLocation(0,0);
-            }else{
-                target.setLocation(x, y);
-            }
-            target.getParent().repaint();
-
-            try{
-                Thread.sleep(20);
-            }catch (InterruptedException e){
-                target.setLocation(0,0);
-                target.getParent().repaint();
-                try{
-                    sleep(500);
-                }catch (InterruptedException ex){
-                    return;
-                }
-            }
-        }
-    }
-}
-
-
+import java.awt.event.*;
+import javax.swing.*;
 
 public class BulletGameFrame extends JFrame{
-        public BulletGameFrame(){
-            setTitle("");
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            GamePanel gamePanel = new GamePanel();
-            setContentPane(gamePanel);
-            setSize(300,300);
-            setVisible(true);
-            gamePanel.startGame();
-        }
+	public BulletGameFrame() {
+		setTitle("사격 게임");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		GamePanel p = new GamePanel();
+		setContentPane(p);
+		setSize(300,300);
+		setResizable(false);		
+		setVisible(true);
+		p.startGame();
+	}
+	
+	public static void main(String [] args) {
+		new BulletGameFrame();
+	}
+}
+class GamePanel extends JPanel {
+	private TargetThread targetThread=null;
+	private JLabel baseLabel = new JLabel();
+	private JLabel bulletLabel = new JLabel();
+	private JLabel targetLabel;
+	
+	public GamePanel() {
+		setLayout(null);
+	
+		baseLabel.setSize(40,40);
+		baseLabel.setOpaque(true);
+		baseLabel.setBackground(Color.BLACK);
 
-    public static void main(String[] args) {
-        new BulletGameFrame();
-    }
+		ImageIcon img = new ImageIcon("img/chicken.png");
+		targetLabel = new JLabel(img);
+		targetLabel.setSize(img.getIconWidth(),img.getIconWidth());
+
+		bulletLabel.setSize(10,10);
+		bulletLabel.setOpaque(true);
+		bulletLabel.setBackground(Color.RED);
+		add(baseLabel);
+		add(targetLabel);
+		add(bulletLabel);
+		
+		// 이 패널에 마우스를 클릭하면 baseLabel이 키 입력을 받을 수 있도록 포커스를 강제 지정
+		this.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				baseLabel.setFocusable(true);
+				baseLabel.requestFocus(); // 키보드 입력을 받도록 포커스 강제 지정			
+			}
+		});
+	}
+	
+	public void startGame() {
+		baseLabel.setLocation(this.getWidth()/2-20, this.getHeight()-40);
+		bulletLabel.setLocation(this.getWidth()/2 - 5, this.getHeight()-50);			
+		targetLabel.setLocation(0, 0);
+		
+		targetThread = new TargetThread(targetLabel);
+		targetThread.start();
+		
+		baseLabel.setFocusable(true);
+		baseLabel.requestFocus(); // 키보드 입력을 받도록 포커스 강제 지정			
+		baseLabel.addKeyListener(new KeyAdapter() {
+			BulletThread  bulletThread = null;
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyChar() == '\n') {
+					if(bulletThread==null || !bulletThread.isAlive()) {
+						bulletThread = new BulletThread(bulletLabel, targetLabel, targetThread);
+						bulletThread.start();
+					}
+				}
+			}
+		});
+	}
+	
+	class TargetThread extends Thread {
+		private JComponent target;
+		public TargetThread(JComponent target) {
+			this.target = target;
+			target.setLocation(0, 0);
+			target.getParent().repaint();
+		}	
+		
+		@Override
+		public void run() {
+			while(true) {
+				int x = target.getX()+5;
+				int y = target.getY();
+				if(x > GamePanel.this.getWidth()) 
+					target.setLocation(0,0);
+				else 
+					target.setLocation(x, y);
+
+				target.getParent().repaint();
+				try {
+					sleep(20);
+				}
+				catch(InterruptedException e) {
+					// the case of hit by a bullet
+					target.setLocation(0, 0);
+					target.getParent().repaint();
+					try {
+						sleep(500); // 0.5초 기다린 후에 계속한다.
+					}catch(InterruptedException e2) {}					
+				}
+			}
+		}			
+	}
+	
+	class BulletThread extends Thread {
+		private JComponent bullet, target;
+		private Thread targetThread;
+		public BulletThread(JComponent bullet, JComponent target, Thread targetThread) {
+			this.bullet = bullet;
+			this.target = target;
+			this.targetThread = targetThread;				
+		}
+		
+		@Override
+		public void run() {
+			while(true) {
+				// 명중하였는지 확인
+				if(hit()) {
+					targetThread.interrupt();
+					bullet.setLocation(bullet.getParent().getWidth()/2 - 5, bullet.getParent().getHeight()-50);						
+					return;
+				}
+				else {
+					int x = bullet.getX() ;
+					int y = bullet.getY() - 5;
+					if(y < 0) {
+						bullet.setLocation(bullet.getParent().getWidth()/2 - 5, bullet.getParent().getHeight()-50);
+						bullet.getParent().repaint();
+						return; // thread ends
+					}
+					bullet.setLocation(x, y);
+					bullet.getParent().repaint();
+				}
+				try {
+					sleep(20);
+				}
+				catch(InterruptedException e) {}
+			}
+		}
+		
+		private boolean hit() {
+			if(targetContains(bullet.getX(), bullet.getY()) || 
+					targetContains(bullet.getX() + bullet.getWidth() - 1, bullet.getY()) ||
+					targetContains(bullet.getX() + bullet.getWidth() - 1, bullet.getY()+bullet.getHeight() - 1) ||
+					targetContains(bullet.getX(), bullet.getY()+bullet.getHeight() - 1))
+				return true;
+			else
+				return false;					
+		}
+		
+		private boolean targetContains(int x, int y) {
+			if(((target.getX() <= x) && (target.getX() + target.getWidth() - 1 >= x)) &&
+					((target.getY() <= y)&& (target.getY() + target.getHeight() - 1 >= y))) {
+				return true;
+			}
+			else
+				return false;
+			
+		}
+	}
 }
