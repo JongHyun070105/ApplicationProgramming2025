20563cd638d90de3337c6b25b1543ba9df4f9e47
JongHyun070105 <bobi09361@gmail.com>
2025-05-07 11:59:21 +0900

    ApplicationProgramming2025

---
+++ src/Day0507/license/soutPrinln.java
@@ -0,0 +1,135 @@
+package Day0507.license;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class soutPrinln {
+    public static void main(String[] args) {
+//        System.out.println("첫 java 프로그램입니다.");
+//        System.out.println("화면에 출력하고 있습니다.");
+//        System.out.println("홍\n길\n동");
+
+//        System.out.println(82 + 27);
+//        System.out.println("82 + 27 = " + (82 + 27));
+
+//        int x = 63;
+//        int y = 18;
+
+//        double x = 63.8;
+//        double y =18.3;
+//
+//        System.out.println("x 값은 " + x + "입니다.\ny 값은 " + y +"입니다.\n합계는 " + (x+y) + "입니다.\n평균은 " + (x + y)/2 +"입니다.");
+
+        Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("정수값을 입력해주세요 >> ");
+//        int input = scan.nextInt();
+//        System.out.println("정수 " + input + "을 입력하였습니다.");
+//        System.out.println("10을 더한 값은 " + (input + 10) + "입니다.");
+//        System.out.println("10을 더한 값은 " + (input - 10) + "입니다.");
+//
+//        System.out.println("마지막 자릿수를 제외한 값은 " + (input / 10) + "입니다.");
+//        System.out.println("마지막 자릿수 값은 " + (input % 10) + "입니다.");
+
+//        System.out.println("밑변의 값 입력 >> ");
+//        double weight = scan.nextDouble();
+//        System.out.println("높이의 값 입력 >> ");
+//        double height = scan.nextDouble();
+//        System.out.println("넓이는 " + (weight * height)/2 + "입니다.");
+
+//        final double PI = 3.1416;
+
+//        System.out.println("반지름 값 입력 >> ");
+//        double harf = scan.nextDouble();
+//        System.out.println("겉넓이 값 : " + (4 * PI * harf * harf)); // 4 * PI * r * r
+//        System.out.println("부피 : " + (4.0 / 3.0 * PI * harf * harf * harf));
+
+//        int a[] = new int[5];
+//        int i;
+//        for(i = 0; i < 5; i++){
+//            a[i] = i + 10;
+//        }
+//        for(i = 0; i < 5; i++){
+//            System.out.printf("%4d", a[i]);
+//        }
+//        System.out.println("\n");
+//        for(i = 0; i < 5; i++){
+//            System.out.printf("%2d", a[i]);
+//        }
+
+//        int[] a = {90,100,80,70,60,50,30};
+//        int hap = 0;
+//        float avg;
+//        for(int i : a){
+//            hap = hap + i;
+//        }
+//        avg = (float)hap / a.length;
+//        System.out.printf("%4d, %4.2f", hap, avg);
+
+//        String str = "Information!";
+//        int n = str.length();
+//        char[] st = new char[n];
+//        n--;
+//        for(int k = n; k >= 0; k--){
+//            st[n-k] = str.charAt(k);
+//        }
+//
+//        for(char k : st){
+//            System.out.printf("%c", k);
+//        }
+
+//        int cnt = 0;
+//        int sum = 0;
+//        for(int i = 0; i <= 7; i++){
+//            if(i % 2 == 1) {
+//                cnt += i;
+//                sum += i;
+//            }
+//        }
+//        System.out.printf(cnt + ", " + sum);
+
+//        String weeks[] = {"월요일","화요일","수요일","목요일","금요일","토요일","일요일"};
+//        int cnt = 1;
+//        for(int i = 1; i < 7; i++){
+//            if(i % 3 == 0){
+//                break;
+//            }
+//            cnt ++;
+//        }
+//        System.out.println("오늘은 " + weeks[cnt]);
+
+//        int arr[] = {0,1,2,3};
+//        for(int num : arr){
+//            System.out.println(num);
+//        }
+
+//        switch ((int) Math.signum(-100)){
+//            case -1 :
+//                System.out.println("N");
+//                break;
+//            case 0 :
+//                System.out.println("P");
+//                break;
+//            case 1 :
+//                System.out.println("E");
+//                break;
+//            default:
+//                System.out.println("Z");
+//        }
+
+//        int x = 3;
+//        int y = 6;
+//        int r;
+//        ClassA cal = new ClassA();
+//        r = cal.funcAdd(x,y);
+//        System.out.print(r);
+//    }
+    }
+}
+
+//class ClassA{
+//    int a = 10;
+//    int funcAdd(int x, int y){
+//        return x + y + a;
+//    }
+//}
