1bf1cf74887fcbfd9bc91c2ee27ae3763b69ce19
JongHyun070105 <bobi09361@gmail.com>
2025-05-13 15:48:31 +0900

    ApplicationProgramming2025

--- src/license/Day0513/MathLearn.java
+++ src/license/Day0513/MathLearn.java
@@ -5,9 +5,9 @@ public class MathLearn {
         System.out.println(Math.abs(-7)); // 절대값
         System.out.println(Math.abs(7)); // 절대값
 
-        System.out.println(Math.signum(-7)); // 양수면 1, 음수면 -1, 0이면 0을 반환
-        System.out.println(Math.signum(7)); // 양수면 1, 음수면 -1, 0이면 0을 반환
-        System.out.println(Math.signum(0)); // 양수면 1, 음수면 -1, 0이면 0을 반환
+        System.out.println(Math.signum(-7)); // 양수면 1, 음수면 -1, 0이면 0을 반환 -> 실수값으로 나옴
+        System.out.println(Math.signum(7)); // 양수면 1, 음수면 -1, 0이면 0을 반환 -> 실수값으로 나옴
+        System.out.println(Math.signum(0)); // 양수면 1, 음수면 -1, 0이면 0을 반환 -> 실수값으로 나옴
 
         System.out.println(Math.min(7,10)); // 최소값
         System.out.println(Math.max(7,10)); // 최대값
@@ -28,5 +28,44 @@ public class MathLearn {
         System.out.println(Math.sqrt(25)); // 실수값, 루트(제곱근)
 
         System.out.println(Math.cbrt(1000)); // 실수값, 세제곱근
+
+        System.out.println(Math.pow(2,5)); // 2*2*2*2*2 -> 실수값으로 나옴
+
+        System.out.println(Math.random()); // 0 ~ 1보다 작은 소수점
+
+        double x = Math.random() * 9 + 1 ; // 1 ~ 10 이하
+        System.out.printf("%8.2f",x); // 왼쪽에서 8칸 띄어쓰고, 소수점 2자리까지만 출력
+
+        double y = (int)(Math.random() * 3);
+        switch ((int) y){
+            case 0 :
+                System.out.println("\n 가위");
+                break;
+            case 1 :
+                System.out.println("\n 바위");
+                break;
+            case 2 :
+                System.out.println("\n 보");
+                break;
+        }
+
+        int arr[] = {0,1,2,3};
+        for (int num : arr){
+            System.out.println(num);
+        }
+
+        switch ((int) Math.signum(-100)){
+            case -1 :
+                System.out.println("N");
+                break;
+            case 0 :
+                System.out.println("P");
+                break;
+            case 1 :
+                System.out.println("E");
+                break;
+            default:
+                System.out.println("Z");
+        }
     }
 }
