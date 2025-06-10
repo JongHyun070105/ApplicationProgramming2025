97e2b2daf5f656cb2db7bb29a5c0a731310f0991
JongHyun070105 <bobi09361@gmail.com>
2025-06-10 19:06:16 +0900

    ApplicationProgramming2025

--- src/license/Day0610/Exam01.java
+++ src/license/Day0610/Exam01.java
@@ -1,5 +1,37 @@
 package license.Day0610;
 
-public class Exam01 {
+//public class Exam01 {
+//    public static void main(String[] args) {
+//        int num[] = {2,1,3,7,4,9};
+//        int numb[] = new int[10];
+//        for(int i = 0; i < num.length; i++){
+//            numb[i] = num[i];
+//        }
+//        for(int i : numb){
+//            System.out.print(i);
+//        }
+//    }
+//}
 
+//public class Exam01 {
+//    public static void main(String[] args) {
+//        int ans = 0;
+//
+//        for(int i = 1; i < 100; i++){
+//            if(i % 3 ==0){
+//                ans++;
+//            } else if(i % 4== 0){
+//                ans--;
+//            }
+//        }
+//        System.out.println(ans);
+//    }
+//}
+
+public class Exam01 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 10;
+        System.out.println(b/a);
+    }
 }
