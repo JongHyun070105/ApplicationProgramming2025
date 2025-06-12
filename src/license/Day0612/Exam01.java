34e272cdca99b9143fc9e4bf1917a2e067fc9a77
JongHyun070105 <bobi09361@gmail.com>
2025-06-12 22:30:12 +0900

    ApplicationProgramming2025

--- src/license/Day0612/Exam01.java
+++ src/license/Day0612/Exam01.java
@@ -1,4 +1,16 @@
 package license.Day0612;
 
 public class Exam01 {
+    public static void main(String[] args) {
+        int number = 1234;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10; // 마지막 자릿수 추출
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10; // 마지막 자릿수 제거
+        }
+
+        System.out.println("역순 출력: " + reversedNumber); // 결과: 4321
+    }
 }
