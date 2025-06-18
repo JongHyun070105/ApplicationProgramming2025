d3690fb9b31bcb3b19eae204503c696fec988b37
JongHyun070105 <bobi09361@gmail.com>
2025-06-18 13:16:47 +0900

    ApplicationProgramming2025

---
+++ src/Day0618/Exam01.java
@@ -0,0 +1,24 @@
+package Day0618;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Exam01 extends JFrame{
+    public Exam01() {
+        setTitle("스윙 컴포넌트 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new FlowLayout());
+
+        contentPane.add(new JButton("클릭"));
+        contentPane.add(new JLabel("라벨입니다"));
+        contentPane.add(new TextField(15));
+
+        setSize(300,150);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Exam01();
+    }
+}
