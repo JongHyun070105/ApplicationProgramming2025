8a8cc6664097df2beecb81427c90cb3cb82ed569
JongHyun070105 <bobi09361@gmail.com>
2025-06-24 15:35:23 +0900

    ApplicationProgramming2025

--- src/Day0624/JSliderEx.java
+++ src/Day0624/JSliderEx.java
@@ -6,31 +6,35 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 
 public class JSliderEx extends JFrame {
-    JSlider js = new JSlider(1, 100, 60);
+    JSlider js = new JSlider(1, 100, 50);
     JLabel la = new JLabel("I Love Java");
 
     public JSliderEx() {
-        la.setFont(new Font("Gothic", Font.BOLD, 60));
         setTitle("슬라이더로 글자 크기 조정");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container cp = getContentPane();
         cp.setLayout(new BorderLayout());
 
-        js.setMajorTickSpacing(20);
-        js.setMinorTickSpacing(5);
-        js.setPaintLabels(true);
-        js.setPaintTicks(true);
+        la.setHorizontalAlignment(SwingConstants.CENTER);
+        la.setFont(new Font("Gothic", Font.PLAIN, js.getValue()));
 
+        // 슬라이더 속성
+        js.setMajorTickSpacing(20); // 큰 눈금
+        js.setMinorTickSpacing(5); // 작은 눈금
+        js.setPaintLabels(true); // 숫자 레이블 표현
+        js.setPaintTicks(true); // 눈금 표현
+
+        // slider value에 따른 폰트 크기 변경 리스너
         js.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
                 la.setFont(new Font(
-                        "Gothic", Font.BOLD, js.getValue()));
+                        "Gothic", Font.PLAIN, js.getValue()));
             }
         });
 
-
+        // 컨테이너에 추가
         cp.add(la, BorderLayout.CENTER);
         cp.add(js, BorderLayout.NORTH);
 
